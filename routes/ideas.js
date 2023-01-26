23aa155f50982af7371281887c27e3e95368f89d
Brad Traversy <traversymedia@gmail.com>
2023-01-26 10:59:43 -0500

    Handle POST request, add idea

--- routes/ideas.js
+++ routes/ideas.js
@@ -43,4 +43,19 @@ router.get('/:id', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// Add an idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
+
+  res.json({ success: true, data: idea });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
