2f0d508603f7b4d38c1252094b1bd8853a40303d
Brad Traversy <traversymedia@gmail.com>
2023-01-26 11:47:15 -0500

    PUT & DELETE request, update & delete ideas

--- routes/ideas.js
+++ routes/ideas.js
@@ -58,4 +58,36 @@ router.post('/', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// Update idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+// Delete idea
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ success: true, data: {} });
+});
+
 module.exports = router;
