810f906e9fbaed8c222f2923c2454d37722f2b95
Brad Traversy <traversymedia@gmail.com>
2023-01-26 17:34:20 -0500

    Client setup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,9 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body parser middleware
 app.use(express.json());
