e985bf6a7edf27677e39bd18fc4d9856ea42f708
Brad Traversy <traversymedia@gmail.com>
2023-01-27 09:00:37 -0500

    IdeaForm component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,6 +8,7 @@ class Modal {
   addEventListeners() {
     this._modalBtn.addEventListener('click', this.open.bind(this));
     window.addEventListener('click', this.outsideClick.bind(this));
+    document.addEventListener('closemodal', () => this.close());
   }
 
   open() {
