ed37965338a7706a400c292ad37f028f6c2b5ed1
Brad Traversy <traversymedia@gmail.com>
2023-01-28 10:16:51 -0500

    Added delete functionality

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -15,6 +15,16 @@ class IdeaList {
     this._validTags.add('inventions');
   }
 
+  addEventListeners() {
+    this._ideaListEl.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-times')) {
+        e.stopImmediatePropagation();
+        const ideaId = e.target.parentElement.parentElement.dataset.id;
+        this.deleteIdea(ideaId);
+      }
+    });
+  }
+
   async getIdeas() {
     try {
       const res = await IdeasApi.getIdeas();
@@ -22,6 +32,17 @@ class IdeaList {
       this.render();
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async deleteIdea(ideaId) {
+    try {
+      // Delete from server
+      const res = await IdeasApi.deleteIdea(ideaId);
+      this._ideas.filter((idea) => idea._id !== ideaId);
+      this.getIdeas();
+    } catch (error) {
+      alert('You can not delete this resource');
     }
   }
 
@@ -45,9 +66,13 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {
         const tagClass = this.getTagClass(idea.tag);
+        const deleteBtn =
+          idea.username === localStorage.getItem('username')
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`
+            : '';
         return `
-      <div class="card">
-      <button class="delete"><i class="fas fa-times"></i></button>
+      <div class="card" data-id="${idea._id}">
+     ${deleteBtn}
       <h3>
         ${idea.text}
       </h3>
@@ -60,6 +85,7 @@ class IdeaList {
       `;
       })
       .join('');
+    this.addEventListeners();
   }
 }
 
--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -12,6 +12,21 @@ class IdeasApi {
   createIdea(data) {
     return axios.post(this._apiUrl, data);
   }
+
+  updateIdea(id, data) {
+    return axios.put(`${this._apiUrl}/${id}`, data);
+  }
+
+  deleteIdea(id) {
+    const username = localStorage.getItem('username')
+      ? localStorage.getItem('username')
+      : '';
+    return axios.delete(`${this._apiUrl}/${id}`, {
+      data: {
+        username,
+      },
+    });
+  }
 }
 
 export default new IdeasApi();
