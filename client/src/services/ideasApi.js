aada313a467864a288d52e039760a33d6ccb1d80
Brad Traversy <traversymedia@gmail.com>
2023-01-28 11:24:44 -0500

    Fixed api url for prod

--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 class IdeasApi {
   constructor() {
-    this._apiUrl = 'http://localhost:5000/api/ideas';
+    this._apiUrl =
+      process.env.NODE_ENV === 'production'
+        ? '/api/ideas'
+        : 'http://localhost:5000/api/ideas';
   }
 
   getIdeas() {
