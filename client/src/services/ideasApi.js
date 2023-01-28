faca719eca72fa41696944f4cb95c1a89ba68a03
Brad Traversy <traversymedia@gmail.com>
2023-01-28 12:45:46 -0500

    Added devServer proxy

--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 class IdeasApi {
   constructor() {
-    this._apiUrl =
-      process.env.NODE_ENV === 'production'
-        ? '/api/ideas'
-        : 'http://localhost:5000/api/ideas';
+    this._apiUrl = '/api/ideas';
 
     console.log(process.env.NODE_ENV);
   }
