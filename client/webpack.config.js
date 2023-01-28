0c371b8cd56e609d91f91e44abe3aacf57bb05e2
Brad Traversy <traversymedia@gmail.com>
2023-01-28 12:36:03 -0500

    prod build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../public'),
